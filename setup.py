92f67e585efdaa693aed50a865f80f8efe4dbbd2
shaunsephton <shaunsephton@gmail.com>
2010-07-19 12:46:51 +0200

    remed readme. version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya-banner',
-    version='0.0.2',
+    version='0.0.3',
     description='Panya banner app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
